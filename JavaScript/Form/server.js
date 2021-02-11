bedb27e388bcd072519af94eb2cefd74c6eebafa
MikeHermsen <68249431+MikeHermsen@users.noreply.github.com>
2021-02-11 22:21:03 +0100

    Update server.js

--- JavaScript/Form/server.js
+++ JavaScript/Form/server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({extended: false}));
 
 
 app.get('/', (req, res) => {
-    res.render('login.ejs', { name : 'Mikey_'});
+    res.render('forum.ejs', { name : 'Mikey_'});
 });
 
 app.listen(3000);
